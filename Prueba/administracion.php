1f07d500b7e2658fc666a59a0f71ef745c099e62
FranRomero93 <FranRomero93>
2017-03-08 12:12:15 +0100

    Administracion y borrar

--- Prueba/administracion.php
+++ Prueba/administracion.php
@@ -6,7 +6,7 @@
 <html class="no-js" lang="">
  
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Biblioteca Virtual</title> 
@@ -76,13 +76,14 @@
                     <table class="table table-hover" class="table">
                         <thead>
                             <tr>
-                            <th>Id_usuario</th>
+                            <th>Id usuario</th>
                             <th>Nombre</th>
                             <th>Apellidos</th>
                             <th>DNI</th>
                             <th>Mail</th>
                             <th>Teléfono</th>
                             <th>Direccion</th>
+                            <th>Nivel de Usuario</th>
                             <th>Editar</th>
                             <th>Borrar</th>
                         </thead>
@@ -98,9 +99,94 @@
                                     echo "<td>".$obj->email."</td>";
                                     echo "<td>".$obj->telefono."</td>";
                                     echo "<td>".$obj->direccion."</td>";
-                                    echo "<td><img src='img/editar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
-                                    echo "<td><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
-                      
+                                    echo "<td>".$obj->nivel_usuario."</td>";
+                                    echo "<td><a href='editar.php?tabla=usuarios&campo=id_usuario&id=$obj->id_usuario'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                    echo "<td><a href='borrar.php?tabla=usuarios&campo=id_usuario&id=$obj->id_usuario'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                echo "</tr>";
+                            }
+
+                            $result->close();
+                            unset($obj);
+                            unset($result);
+                            unset($query);
+                        } 
+
+                        ?>
+                        </tbody>
+                    </table>
+            </div>
+            <div id="libro" class="table-responsive">
+                <h2>Libros</h2>
+                <?php
+                    $query="SELECT * from libro";
+                    if ($result = $connection->query($query)) {
+                ?>
+
+                    <table class="table table-hover" class="table">
+                        <thead>
+                            <tr>
+                            <th>Id libro</th>
+                            <th>Id autor</th>
+                            <th>Id categoría</th>
+                            <th>Titulo</th>
+                            <th>Editorial</th>
+                            <th>Descripción</th>
+                            <th>Disponibles</th>
+                            <th>Editar</th>
+                            <th>Borrar</th>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                            while($obj = $result->fetch_object()) {
+                                echo "<tr>";
+                                    echo "<td>".$obj->id_libro."</td>";
+                                    echo "<td>".$obj->id_autor."</td>";
+                                    echo "<td>".$obj->id_categoria."</td>";
+                                    echo "<td>".$obj->titulo."</td>";
+                                    echo "<td>".$obj->editorial."</td>";
+                                    echo "<td>".$obj->descripcion."</td>";
+                                    echo "<td>".$obj->disponibles."</td>";
+                                    echo "<td><a href='editar.php?tabla=libro&campo=id_libro&id=$obj->id_libro'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                    echo "<td><a href='borrar.php?tabla=libro&campo=id_libro&id=$obj->id_libro'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                echo "</tr>";
+                            }
+
+                            $result->close();
+                            unset($obj);
+                            unset($result);
+                        } 
+
+                        ?>
+                        </tbody>
+                    </table>
+            </div> 
+            <div id="autor" class="table-responsive">
+                <h2>Autores</h2>
+                <?php
+                    $query="SELECT * from autor";
+                    if ($result = $connection->query($query)) {
+                ?>
+
+                    <table class="table table-hover" class="table">
+                        <thead>
+                            <tr>
+                            <th>Id autor</th>
+                            <th>Nombre</th>
+                            <th>Apellidos</th>
+                            <th>Editar</th>
+                            <th>Borrar</th>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                            while($obj = $result->fetch_object()) {
+                                echo "<tr>";
+                                    echo "<td>".$obj->id_autor."</td>";
+                                    echo "<td>".$obj->nombre."</td>";
+                                    echo "<td>".$obj->apellidos."</td>";
+                                    echo "<td><a href='editar.php?tabla=autor&campo=id_autor&id=$obj->id_autor'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                    echo "<td><a href='borrar.php?tabla=autor&campo=id_autor&id=$obj->id_autor'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
                                 echo "</tr>";
                             }
 
@@ -113,9 +199,42 @@
                         </tbody>
                     </table>
             </div>
-            <div id="">
-                
-            </div>            
+            <div id="categoria" class="table-responsive">
+                <h2>Categorias</h2>
+                <?php
+                    $query="SELECT * from categoria";
+                    if ($result = $connection->query($query)) {
+                ?>
+
+                    <table class="table table-hover" class="table">
+                        <thead>
+                            <tr>
+                            <th>Id categoria</th>
+                            <th>Nombre de categoría</th>
+                            <th>Editar</th>
+                            <th>Borrar</th>
+                        </thead>
+                        <tbody>
+
+                        <?php
+                            while($obj = $result->fetch_object()) {
+                                echo "<tr>";
+                                    echo "<td>".$obj->id_categoria."</td>";
+                                    echo "<td>".$obj->nombre_categoria."</td>";
+                                    echo "<td><a href='editar.php?tabla=categoria&campo=id_categoria&id=$obj->id_categoria'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                    echo "<td><a href='borrar.php?tabla=categoria&campo=id_categoria&id=$obj->id_categoria'><img src='img/borrar.jpg' class='img-responsive' alt='Imagen responsive' width='30pt' height='30pt'></img></td>";
+                                echo "</tr>";
+                            }
+
+                            $result->close();
+                            unset($obj);
+                            unset($result);
+                        }
+
+                        ?>
+                        </tbody>
+                    </table>
+            </div>
         </div>
     </div>
 </body>
---
+++ Prueba/editar.php
@@ -0,0 +1,140 @@
+<?php
+      session_start();
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+ 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biblioteca Virtual</title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="../Prueba/js/jquery-3.1.1.js"></script>
+    
+    
+    <?php
+        include_once ('library/conexion-bd.php');
+    ?>
+</head>
+
+<body>
+    <div class="container background">
+        <div id="header" class="header">
+            <h1><a href="index.php">Biblioteca Virtual</a><small> Registro</small></h1>
+        </div>
+        <div id="nav-">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="index.php">BV</a>
+                    </div>
+                        <ul class="nav navbar-nav">
+                        </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+			    		<h3 class="panel-title">Regístrese <small>It's free!</small></h3>
+			 			</div>
+			 			<div class="panel-body">
+			    		<form role="form" action="registro.php" method="post">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			                <input type="text" name="user" class="form-control input-sm" placeholder="Nombre" required>
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="apellidos" class="form-control input-sm" placeholder="Apellidos" required>
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div class="form-group">
+			    				<input type="email" name="mail"  class="form-control input-sm" placeholder="Email" required>
+			    			</div>
+
+                            <div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="dni" class="form-control input-sm" placeholder="DNI" required>
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="tel" name="telefono" class="form-control input-sm" placeholder="Teléfono" required>
+			    					</div>
+			    				</div>
+			    			</div>
+                            
+                            <div class="form-group">
+			    				<input type="text" name="direccion" class="form-control input-sm" placeholder="Dirección" required>
+			    			</div>
+                            
+                            <div class="form-group">
+                                <input type="password" name="password"  class="form-control input-sm" placeholder="Password" required>
+                            </div>
+			    			
+			    			<input type="submit" value="Registro" class="btn btn-info btn-block">
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
+    </div>
+    
+    <?php 
+        if (isset($_POST["user"])) {
+                    
+            $user=$_POST["user"];
+            $apellidos=$_POST["apellidos"];
+            $dni=$_POST["dni"];
+            $mail=$_POST["mail"];
+            $telefono=$_POST["telefono"];
+            $direccion=$_POST["direccion"];
+            $password=$_POST["password"];
+
+            $consulta="alter table usuarios (nombre, apellidos, dni, email, telefono, direccion, password) values ('$user', '$apellidos', '$dni', '$mail', $telefono, '$direccion', md5('$password'));";
+
+
+            if ($result = $connection->query($consulta)) {
+
+              //No rows returned
+              if ($result->num_rows===0) {
+                echo "REGISTRO INVALIDO";
+              } else {
+                //VALID LOGIN. SETTING SESSION VARS
+                $_SESSION["user"]=$_POST["user"];
+
+                header("Location: index.php");
+              } 
+
+            }else {
+                    echo "Wrong Query";
+                    var_dump($consulta);
+            }
+        }
+    ?>
+    
+</body>
+</html>
+
+
+
+
+$query="select * from ".$_GET['tabla']." where ".$_GET['campo']."='".$_GET['id']."'";
+        echo ($query);
+        if ($result = $connection->query($query)) {
